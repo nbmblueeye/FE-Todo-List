b11161ca70aa2f52bc5948b46e2d486f5d629f82
nguyenbinhminh85 <nguyenbinhminh85@gmail.com>
2024-04-08 14:08:58 +0700

    prepare to server

--- frontend/src/features/api/taskApi.tsx
+++ frontend/src/features/api/taskApi.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/todos/";
+const API_URL = "https://fe-todo-list.onrender.com/api/todos/";
 
 const getTasks = async (slug:string) => {
     let filter = {}
